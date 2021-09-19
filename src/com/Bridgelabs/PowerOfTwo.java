ea61af764fc12fbf62072cdf096951928a564934
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-19 22:36:51 +0530

    Add:-Printing Table of Two till user InputValue

---
+++ src/com/Bridgelabs/PowerOfTwo.java
@@ -0,0 +1,22 @@
+package com.Bridgelabs;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter value for print table of power of 2: ");
+		
+		long power = scan.nextLong();
+		int number =1, i;
+		
+		
+		for ( i =1;  i <= power; i++) {
+			number = number * 2; 
+			System.out.println("2 ^ " + i + " = " + number);
+		}
+
+	}
+
+}
