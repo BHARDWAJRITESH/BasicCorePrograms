d3fd30ba95dcbadb421e0629477c5303ed72bdf6
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-19 20:04:55 +0530

    Add:-To determine the leap year

---
+++ src/com/Bridgelabs/LeapYear.java
@@ -0,0 +1,25 @@
+package com.Bridgelabs;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		System.out.println("Enter the year: ");
+		
+		int year = num.nextInt();
+		
+		if(year % 400 == 0) {
+			System.out.println(year + " is a leap year");
+		}else if(year % 100 ==0) {
+			System.out.println(year + " is  not a leap year");
+		}else if (year % 4 == 0) {
+			System.out.println(year + " is a leap year");
+		}else {
+			System.out.println(year + " is not a leap year");
+		}
+
+	}
+
+}
